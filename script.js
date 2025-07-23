2d077555f63fe01a65116d5ff61cec4e04214877
hpnos <40741874+hpnos@users.noreply.github.com>
2025-07-22 21:25:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ const sounds = {
   "images/42 syivie.png": new Audio("sounds/42.mp3"),
   "images/47 galacta.png": new Audio("sounds/47.mp3"),
   "images/36 ariana.png": new Audio("sounds/36.mp3"),
-  "images/44 morgan.png": new Audio("sounds/36.mp3"),
+  "images/44 morgan.png": new Audio("sounds/44.mp3"),
   //"images/48 agatha.png": new Audio("sounds/48.mp3"),
   //"images/49 yuk.png": new Audio("sounds/49.mp3"),
   "images/50 magik.png": new Audio("sounds/50.mp3"),
